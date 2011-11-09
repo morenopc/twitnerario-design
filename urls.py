8385b2bf7281c8fdb9afe7bd6532a2d51832be18
Moreno <moreno.pinheiro@gmail.com>
2011-11-09 21:52:07 -0200

    first commit

---
+++ urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+#from twitterauth.views import twitter_signin, twitter_return
+#from core.tweet import pontos, linhas
+admin.autodiscover()
+
+urlpatterns=patterns('',
+    url(r'^$',include('registros.urls', namespace='registro')),
+    url(r'^admin/', include(admin.site.urls)),
+    
+    #url('^login/$', twitter_signin, name='login'),
+    #url('^return/$', twitter_return, name='return'),
+    (r'^registro/', include('registros.urls', namespace='registro')),
+
+    #url(r'^pontos/$', pontos, name='pontos'),
+    #url(r'^(\d+)/linhas/$', linhas, name='linhas'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+    )
